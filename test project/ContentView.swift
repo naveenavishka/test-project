e2769c09b428a87d416c11eca2013c828e085123
naveenavishka <naveen.avishka@outlook.com>
2024-06-19 11:16:16 +0530

    Initial Commit

---
+++ test project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test project
+//
+//  Created by Hayleys Advantis ISD on 2024-06-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test project/test_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_projectApp.swift
+//  test project
+//
+//  Created by Hayleys Advantis ISD on 2024-06-19.
+//
+
+import SwiftUI
+
+@main
+struct test_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
